e44761290ab51fcccaa3093a73432dff48e9a131
olivier <olivier.evalet@isb-sib.ch>
2014-10-20 17:49:22 +0200

    move project config outside the module

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,19 @@
 (function (ng, undefined) {'use strict';
-	angular.module('main', ['ngResource','npHelp']);
+	angular.module('main', ['ngResource','npHelp'])
+
+	.config(function($routeProvider, $locationProvider, $provide) {
+		// Use the bang prefix for Google ajax crawlability
+		// https://developers.google.com/webmasters/ajax-crawling/docs/specification?csw=1
+
+
+		// Hashbang in HTML5 Mode
+		$locationProvider.html5Mode(false);
+		$locationProvider.hashPrefix('!');
+
+		$routeProvider
+			.when('/', { templateUrl: "html/np-help.intro.html" })
+			.when('/:entity', { templateUrl: "html/np-help.element.html"})
+			.otherwise({ redirectTo: '/' });
+		
+	});  	
 })(angular);
